cf061fdf29debfffc5e8d384e876bc8a73911277
sumanth <bhumisumanth299@gmail.com>
2024-05-20 12:53:32 +0530

    add github copilot

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -137,4 +137,53 @@ return {
       }
     end,
   },
+
+  {
+    "zbirenbaum/copilot.lua",
+    config = function()
+      require("copilot").setup {
+        panel = {
+          enabled = false,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>",
+          },
+          layout = {
+            position = "bottom", -- | top | left | right
+            ratio = 0.4,
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-i>",
+            accept_word = "<M-;>",
+            accept_line = "<M-o>",
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = "node", -- Node.js version must be > 18.x
+        server_opts_overrides = {},
+      }
+    end,
+  },
 }
